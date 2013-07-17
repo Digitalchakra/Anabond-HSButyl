d517b42018b5ecc2a0a63102ff167160cbd2bf5f
dc-mani <manikandan@digitalchakra.in>
2013-07-17 18:27:40 +0530

    product page to contact page with product nam

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -97,11 +97,11 @@ class Contact extends CI_Controller {
 		{
 			if(isset($_POST['product_name']) && ($_POST['product_name']))
 	
-{
-		$data['product_name']=$_POST['product_name'];
-			$data['product_image']=$_POST['product_image'];
+			{
+				$data['product_name']=$_POST['product_name'];
+				$data['product_image']=$_POST['product_image'];
+			}
 			$data['product']=$this->product_model->get_products();
-}
 			$words = file("./captcha_txt/google_captcha.txt"); 
 			$word = trim($words[rand(0, count($words) - 1)])." ".rand(0,999);
 			$vals = array(
