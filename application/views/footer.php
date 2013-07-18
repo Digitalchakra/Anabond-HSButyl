3f3c3768e33c562d824a9b7552d8f37bbcff135b
Balaji <balaji.sp@digitalchakra.in>
2013-07-18 17:57:04 +0530

    Completed changes on footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,5 @@
 <!--     footer    -->
-   
+   <link rel = "stylesheet" media = "screen" href = "<?=base_url('assets/css/sss.css');?>" />
    <div class="footer">
    		<div class="cont">
       		<a href="http://digitalchakra.in/" target="_blank"><img src="<?=base_url('assets/images/logo2.png');?>" alt="Digital Chakra" title="Digital Chakra"/></a>
@@ -7,7 +7,7 @@
           <div class="foot_lft">
           
           <p>
-          &copy; Copyrights Anabond HSButyl All Rights Reserved | Privacy Policy</p>
+          &copy; Copyright 2013 &nbsp;|&nbsp;&nbsp;<a href="http://www.anabond.com" target="_blank">Anabond</a>&nbsp;<a href="http://www.hsbutyl.com" target="_blank">HSButyl</a>|&nbsp;&nbsp;All Rights Reserved&nbsp;&nbsp;|&nbsp; <a href="#">Privacy Policy</a></p>
           </div>
       </div>
    </div>
