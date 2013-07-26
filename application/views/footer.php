1a868c744cda0103d20460354171b517c21f4147
dc-mani <manikandan@digitalchakra.in>
2013-07-26 17:05:44 +0530

    CSS fix - Balaji

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,8 @@
 <!--     footer    -->
    <link rel = "stylesheet" media = "screen" href = "<?=base_url('assets/css/sss.css');?>" />
+   </div> <!-- This is for footer fixing | overall "all_in" class dont delete-->
    <div class="footer">
+	   <div class="push">
    		<div class="cont">
       		<a href="http://digitalchakra.in/" target="_blank"><img src="<?=base_url('assets/images/logo2.png');?>" alt="Digital Chakra" title="Digital Chakra"/></a>
           <p>Powered by </p>
@@ -9,6 +11,7 @@
           <p>
           &copy; Copyright <?=date('Y');?> &nbsp;|&nbsp;&nbsp;<a href="http://www.anabond.com" target="_blank">Anabond</a>&nbsp;<a href="http://www.hsbutyl.com" target="_blank">HSButyl</a>|&nbsp;&nbsp;All Rights Reserved&nbsp;&nbsp;|&nbsp; <a href="#">Privacy Policy</a></p>
           </div>
+      </div>
       </div>
    </div>
 </body>    
--- application/views/header.php
+++ application/views/header.php
@@ -14,6 +14,7 @@
 </head>
 
 <body>
+	<div class="all_in">
 	<div class="wrapper">
   	<div class="header">
 		<? $page = $this->uri->segment(1); ?>
