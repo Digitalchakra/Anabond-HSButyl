51263784ad3e1cb1e5cb2136d3ad4b796d2548fb
dc-mani <manikandan@digitalchakra.in>
2013-09-26 17:41:46 +0530

    txt change

--- application/views/about.php
+++ application/views/about.php
@@ -8,7 +8,7 @@
       	<p>Anabond HS Butyl Pvt Ltd was formed in April 2013.  The company, based in India consists of a 50/50 Joint Venture between <a href="http://www.anabond.com/index.htm">Anabond Limited</a> of India and <a href="http://www.hsbutyl.com/">HS Butyl Limited</a> of England. Anabond HS Butyl Pvt Ltd will manufacture butyl tapes in India to serve the Indian market.</p>
       	<p>HS Butyl has five decades of experience in manufacturing butyl tapes in the United Kingdom. Their products are sold in over 40 countries worldwide and the quality of butyl tapes is accredited with the ISO: 9001 standard. HS Butyl’s experience allows their products to be manufactured for a range of applications and in different formats including double-sided, single-sided, pre-cut lengths and in multilane format.</p>
       	<p>Anabond was established in the year 1979, for developing, manufacturing and marketing of Engineering Adhesives and Sealants. Anabond is the first Indian company to manufacture Anaerobic Adhesives & Sealants, RTV Silicone Sealants and Single component epoxies.</p>
-      	<p>Today Anabond Products include Anaerobic, Epoxies, RTV Silicones, Polyurethanes, Rubber Based (Chloroprene) Butyl tapes, Acrylic, Solvent Based Adhesives, Cyanoacrylate, Specialty Coatings etc. with more than 300 products.</p>
+      	<p>Today Anabond Products include Anaerobic, Epoxies, RTV Silicones, Polyurethanes, Rubber Based (Chloroprene) Butyl tapes, Acrylic, Solvent Based Adhesives, Cyanoacrylate, and Specialty Coatings etc. with more than 300 products.</p>
       	<p>For further information please visit the corporate website at <a href="http://www.anabond.com/">Anabond.com</a></p>
       </div>
     </div>
