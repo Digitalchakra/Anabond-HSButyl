72325e35892892e5d4a94d9f620ebce5a0c5e78b
Manikandan R <manikandan@digitalchakra.in>
2014-04-16 16:17:31 +0530

    dc logo removed - Prabakaran

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,8 +4,8 @@
    <div class="footer">
 	   <div class="push">
    		<div class="cont">
-      		<a href="http://digitalchakra.in/" target="_blank"><img src="<?=base_url('assets/images/logo2.png');?>" alt="Digital Chakra" title="Digital Chakra"/></a>
-          <p>Powered by </p>
+      		<!-- <a href="http://digitalchakra.in/" target="_blank"><img src="<?=base_url('assets/images/logo2.png');?>" alt="Digital Chakra" title="Digital Chakra"/></a>
+          <p>Powered by </p> -->
           <div class="foot_lft">
           
           <p>
